5352e3ba94243897e46561173ae4c606929ee673
msiwy12345 <mateuszsiwy00@gmail.com>
2025-01-22 23:21:17 +0100

    Added inteface for JwtService

--- ChatMeeting.Core.Application/Services/JwtService.cs
+++ ChatMeeting.Core.Application/Services/JwtService.cs
@@ -2,10 +2,12 @@
 using ChatMeeting.Core.Domain.Models;
 using ChatMeeting.Core.Domain.Options;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,9 +24,38 @@ namespace ChatMeeting.Core.Application.Services
 
         public AuthDTO GenerateJwtToken(User user)
         {
+            var claims = GetClaims(user);
+            var expiryDate = DateTime.Now.AddMinutes(_jwtSettingsOptions.ExpiryInMinutes);
+            var creds = GetCredentials();
             var token = new JwtSecurityToken(
-                claims: 
-                )
+                claims: claims, // lista atrybutow ktore beda przechowywane w tokenie, ktore mozemy dekodowac
+                expires: expiryDate, // data wygasniecia
+                signingCredentials: creds // okresla informacje potrzebne do przypisania tokena jwt, zawiera algorytm jaki
+                // zastosujemy do zaszyfrowania tokenu
+                );
+
+            return new AuthDTO
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                ExpiryDate = expiryDate
+            };
+        }
+
+        private SigningCredentials GetCredentials()
+        {
+            var byteSecretKey = Encoding.ASCII.GetBytes(_jwtSettingsOptions.SecretKey );
+            var key = new SymmetricSecurityKey(byteSecretKey);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            return creds;
+        }
+
+        private Claim[] GetClaims(User user)
+        {
+            return new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(JwtRegisteredClaimNames.Jti, user.Id.ToString())
+            };
         }
     }
 }
